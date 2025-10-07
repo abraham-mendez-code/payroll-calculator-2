8e961bb535a2fc9f327ac78cf0b0e087235efb01
abraham-mendez-code <abraham.me001@gmail.com>
2025-10-07 14:40:23 -0400

    Added loop to create new employee for each line in csv and print out their info

--- src/main/java/com/pluralsight/PayrollCalculatorApp.java
+++ src/main/java/com/pluralsight/PayrollCalculatorApp.java
@@ -8,7 +8,7 @@ public class PayrollCalculatorApp {
 
     public static void main(String[] args) throws IOException {
 
-        FileReader fileReader = new FileReader("payroll.csv");
+        FileReader fileReader = new FileReader("employees.csv");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
         String input;
@@ -19,23 +19,39 @@ public class PayrollCalculatorApp {
         hoursWorked
         payRate
           */
-        //Employee[] employees = new Employee[];
 
+        Employee[] employees = new Employee[8];
+
+        int i = 0;
         while ( (input = bufferedReader.readLine()) != null ) {
 
+            // split the current line into seperate fields
             String[] currentLine = input.split("[|]");
 
-            for (int i = 0; i < currentLine.length; i++) {
+            // create variables to store each field
+            String id = currentLine[0];
+            String name = currentLine[1];
+            double hoursWorked = Double.parseDouble(currentLine[2]);
+            double payRate = Double.parseDouble(currentLine[3]);
 
-                System.out.println(currentLine[i]);
+            // create a new employee object using the fields
+            employees[i] = new Employee(id, name, hoursWorked, payRate);
 
-            }
+            /*
+                Display the employee using a printf and by calling the employee's getEmployeeId()
+                , getName(), and getGrossPay() methods
+             */
+
+            System.out.printf("ID: %s, Name: %s, Gross Pay: $%.2f\n", employees[i].getEmployeeID(),
+                    employees[i].getName(), employees[i].getGrossPay());
+
+            i++;
 
         }
 
         bufferedReader.close();
 
-        System.out.println(input);
+        employees[7].toString();
 
     }
 
