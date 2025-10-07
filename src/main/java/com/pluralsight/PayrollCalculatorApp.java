86e00ae2cb5a647d0626091ba9bde263ffbf59f9
abraham-mendez-code <abraham.me001@gmail.com>
2025-10-07 13:58:35 -0400

    Added file read loop to PayrollCalculatorApp, currently prints each line split. Printing to be removed

--- src/main/java/com/pluralsight/PayrollCalculatorApp.java
+++ src/main/java/com/pluralsight/PayrollCalculatorApp.java
@@ -11,8 +11,27 @@ public class PayrollCalculatorApp {
         FileReader fileReader = new FileReader("payroll.csv");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        String input = bufferedReader.readLine();
+        String input;
 
+         /*
+         employeeId
+        name
+        hoursWorked
+        payRate
+          */
+        //Employee[] employees = new Employee[];
+
+        while ( (input = bufferedReader.readLine()) != null ) {
+
+            String[] currentLine = input.split("[|]");
+
+            for (int i = 0; i < currentLine.length; i++) {
+
+                System.out.println(currentLine[i]);
+
+            }
+
+        }
 
         bufferedReader.close();
 
